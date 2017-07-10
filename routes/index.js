76e31287cbfc1dd9c31c0a892e2e87dfe6609b7e
Not-A-Sith-Lord <arieldeschapell@gmail.com>
2017-07-10 11:48:49 -0400

    Looping through articles in rss, saving titles, links, and keyword count to array as object

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const request = require("request");
 router.get('/', (req, res, next) => {
 
   var feedparser = new FeedParser([]);
-  const keywords = ["tennis", "baseball", "programming", "soylent", "norway"];
+
 
 
 class Feed {
@@ -45,16 +45,53 @@ class Feed {
     // always handle errors
   });
 
+  var articleArray = [];
+
   feedparser.on('readable', function () {
+
+      const keywords = ["tech", "code", "program", "money", "web"];
     // This is where the action is!
     var stream = this; // `this` is `feedparser`, which is a stream
     var meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
-    var item;
-//???????
-    while (item = stream.read()) {
-      console.log(item);
+    var items;
+
+
+    //???????
+    while (items = stream.read()) {
+
+      let count = 0;
+
+      keywords.forEach((keyword) => {
+        var reg = new RegExp(keyword, "g");
+        console.log(( typeof items.description.match(reg) || []).length);
+        console.log(typeof count);
+        count = count + (items.description.match(reg) || []).length;
+        console.log(`________________________________ ${count}_______________________________`)
+      });
+
+
+
+
+
+      articleArray.push({
+        title: items.title,
+        count: count,
+        uri: items.guid
+      });
+
+// console.log(items.description);
+// console.log(items.title, items.guid);
+
       console.log("________________________________________________________________________________________________________________--");
+
+
+
+
+
+console.log("THE NUMBER OF OCCURENCES IS " + count);
+
     }
+console.log(articleArray);
   });
 
 
